0bb2bddf91850b8fe4b47d3558174af350405a36
mingming <1253706151@qq.com>
2022-02-25 17:55:58 +0800

    bug fixed

--- src/entities/trade.ts
+++ src/entities/trade.ts
@@ -1,7 +1,7 @@
 import { Currency, CurrencyAmount, NFT, Action, BigintIsh } from "@valuemaster/sdk-core";
 
 export interface options {
-  readonly action: Action
+	readonly action: Action
 	readonly quantity: BigintIsh
 }
 
@@ -9,32 +9,57 @@ export class Trade<TInput extends Currency> {
 	public readonly listingId: string
 
 	public readonly nft: NFT
-  /**
-   * The input amount for the trade assuming no slippage.
-   */
-  public readonly inputAmount: CurrencyAmount<TInput>
+	/**
+	 * The input amount for the trade assuming no slippage.
+	 */
+	public readonly inputAmount: CurrencyAmount<TInput>
 
 	/**
 	 * Specified quantity for sale or buy
 	 */
-  public readonly quantity: BigintIsh
+	public readonly quantity: BigintIsh
 
 	/**
 	 * the action is buy | sale | update | cancel
 	 */
 	public readonly action: Action
 
-	public static buy(nft: NFT, opts: options) { return new Trade(nft, opts) }
+	public static buy<TInput extends Currency>(
+		currencyAmount: CurrencyAmount<TInput>,
+		nft: NFT,
+		opts: options
+	) {
+		return new Trade(currencyAmount, nft, opts)
+	}
 
-	public static sale(nft: NFT, opts: options) { return new Trade(nft, opts) }
+	public static sale<TInput extends Currency>(
+		currencyAmount: CurrencyAmount<TInput>,
+		nft: NFT,
+		opts: options
+	) {
+		return new Trade(currencyAmount, nft, opts)
+	}
 
-	public static update(nft: NFT, opts: options) { return new Trade(nft, opts) }
+	public static update<TInput extends Currency>(
+		currencyAmount: CurrencyAmount<TInput>, 
+		nft: NFT, 
+		opts: options
+	) { 
+		return new Trade(currencyAmount, nft, opts) 
+	}
 
-	public static cancel(nft: NFT, opts: options) { return new Trade(nft, opts) }
+	public static cancel<TInput extends Currency>(
+		currencyAmount: CurrencyAmount<TInput>,
+		nft: NFT, 
+		opts: options
+	) { 
+			return new Trade(currencyAmount, nft, opts) 
+	}
 
-  private constructor(nft: NFT, options : options) {
-		  this.nft = nft
-      this.action = options.action
-			this.quantity = options.quantity
-  }
+	private constructor(amount: CurrencyAmount<TInput>, nft: NFT, options: options) {
+		this.inputAmount = amount
+		this.nft = nft
+		this.action = options.action
+		this.quantity = options.quantity
+	}
 }
